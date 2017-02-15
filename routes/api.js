9c3f6e357c6c7198db15e719a9006b154555daa8
thachdd <ddthach95@gmail.com>
2017-02-15 15:55:20 +0700

    Cache data in static file.

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var phimmoi = require('./lib/phimmoi')
 var router = express.Router();
+var fs = require('fs');
+var path = require('path')
 
 var webdriverio = require('webdriverio');
 var options = {
@@ -8,6 +10,22 @@ var options = {
         browserName: 'firefox'
     }
 };
+
+var DATA = {};
+
+var loadData = function() {
+    var filePath = path.join(__dirname, 'data.json');
+    var tmp = fs.readFileSync(filePath, 'utf8');
+    DATA = JSON.parse(tmp);
+};
+
+var storeData = function(episodes) {
+    var filePath = path.join(__dirname, 'data.json');
+    var tmp = {'data': episodes};
+    fs.writeFileSync(filePath, JSON.stringify(tmp));
+}
+
+loadData();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -46,24 +64,28 @@ router.get('/player', function(req, res, next) {
    var id = req.query.id;
 
    if (id) {
-       phimmoi.search().then(function (episodes) {
-           var item = episodes.slice(id-1, id)[0];
-           console.log(item);
+       var item = DATA.data.slice(id-1, id)[0];
 
-           if (item.url) {
-               phimmoi.findMedias(item.url).then(function (episode) {
-                   res.json({'data': episode});
-               });
-           }
-           else {
-               res.json({'data': []});
-           }
-
-       });
+       if (item.url) {
+           phimmoi.findMedias(item.url).then(function (episode) {
+               res.json({'data': episode});
+           });
+       }
+       else {
+           res.json({'data': []});
+       }
    }
    else {
        res.json({'data': []});
    }
+});
+
+router.get('/updateData', function(req, res, next) {
+    phimmoi.search().then(function(episodes) {
+        storeData(episodes);
+        loadData();
+        res.json({'data':'data updated'});
+    });
 });
 
 module.exports = router;
